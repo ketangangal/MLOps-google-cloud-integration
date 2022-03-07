28a6915f4dc3b8b151e52d178f0c9ac376da28ae
ketanGangal <ketangangal98@gmail.com>
2022-03-08 02:14:40 +0530

    google create and upload function added

--- utils/gcp_integration.py
+++ utils/gcp_integration.py
@@ -1,22 +1,23 @@
 from google.cloud import storage
 
 
-def create_bucket_class_location(bucket_name):
-    """
-    Create a new bucket in the US region with the coldline storage
-    class
-    """
-    # bucket_name = "your-new-bucket-name"
+def create_bucket(config=None, storage_client=storage.Client()):
+    try:
+        bucket = storage_client.bucket(config['param']['bucket_name'])
+        bucket.storage_class = config['param']['storage_class']
+        bucket.location = config['param']['location']
+        bucket = storage_client.create_bucket(bucket)
+        return f"Bucket {config['param']['bucket_name']} Created. Status : {bucket}"
+    except Exception as e:
+        return f"An Error Occurred While creating bucket : {e.__str__()}"
 
-    storage_client = storage.Client()
 
-    bucket = storage_client.bucket(bucket_name)
-    bucket.storage_class = "COLDLINE"
-    new_bucket = storage_client.create_bucket(bucket, location="us")
+def upload_to_bucket(config=None, storage_client=storage.Client()):
+    try:
+        bucket = storage_client.get_bucket(config['param']['bucket_name'])
+        blob = bucket.blob(config['param']['blob_name'])
+        blob.upload_from_filename(config['param']['file_path'])
+        return f"Successfully uploaded Run : {config['param']['blob_name']}"
+    except Exception as e:
+        return f"An Error Occurred While Uploading to the bucket : {e.__str__()}"
 
-    print(
-        "Created bucket {} in {} with storage class {}".format(
-            new_bucket.name, new_bucket.location, new_bucket.storage_class
-        )
-    )
-    return new_bucket
